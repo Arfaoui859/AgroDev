3fa26990ccd20bb3943f08971da3dd7d1fc8c8bf
Arfaoui859 <56050779+Arfaoui859@users.noreply.github.com>
2026-02-15 16:20:05 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, Plugin } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { createServer } from "./server";
+
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -15,9 +15,9 @@ export default defineConfig(({ mode }) => ({
     },
   },
   build: {
-    outDir: "dist/spa", // must match vercel.json outputDirectory
+    outDir: "dist", // must match vercel.json outputDirectory
   },
-  plugins: [react(), expressPlugin()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./client"),
